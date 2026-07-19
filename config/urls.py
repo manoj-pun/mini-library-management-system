c21103ef09d92e2c3646c3041b4a51cdcfe7a4ef
manoj-pun <manojpun840@gmail.com>
2026-07-19 16:02:09 +0545

    add views.py in config

--- config/urls.py
+++ config/urls.py
@@ -20,12 +20,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 
-    path("api/token",TokenObtainPairView.as_view(), name="obtain_token"),
+    path("api/token/",TokenObtainPairView.as_view(), name="obtain_token"),
     path("api/token/refresh/",TokenRefreshView.as_view(), name="refresh_token"),
 
     path('api/', include("apps.authors.urls")),
+    path('api/', include("apps.books.urls")),
 ]
